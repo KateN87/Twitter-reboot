d82f6be1cb40d68f96de42158e12f82b652c7968
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-18 12:14:00 +0200

    throw error added

--- backend/src/Routes/logRoutes.js
+++ backend/src/Routes/logRoutes.js
@@ -8,34 +8,39 @@ const router = express.Router();
 
 router.post("/signup", async (req, res) => {
 	const { username, password } = req.body;
+	try {
+		//checks if userName and password is put in
+		if (!username || !password) {
+			throw Error("Please fill in username and password");
+		}
 
-	//checks if userName and password is put in
-	if (!username || !password) {
-		res.send("Please fill in username and password");
+		//checks if userName already exists
+		const maybeUser = db.data.users.find((user) => username === user.username);
+
+		if (maybeUser) {
+			throw Error("User name already taken");
+		}
+
+		//checks if password is strong enough
+		if (!validator.isStrongPassword(password)) {
+			throw Error(
+				"Password needs to be at least 8 characters and contain lower case, upper case, number and special character"
+			);
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(password, salt);
+
+		const newUser = { username, password: hash };
+
+		db.data.users.push(newUser);
+		db.write();
+
+		res.status(200).json({ username });
+	} catch (error) {
+		console.error(error);
+		res.status(400).json({ error: error.message });
 	}
-
-	//checks if userName already exists
-	const maybeUser = db.data.users.find((user) => username === user.username);
-
-	if (maybeUser) {
-		res.send("User name already taken");
-	}
-
-	if (!validator.isStrongPassword(password)) {
-		res.send(
-			"Password needs to be at least 8 characters and contain lower case, upper case, number and special character"
-		);
-	}
-
-	const salt = await bcrypt.genSalt(10);
-	const hash = await bcrypt.hash(password, salt);
-
-	const newUser = { username, password: hash };
-
-	db.data.users.push(newUser);
-	db.write();
-
-	res.send(newUser);
 });
 
 export default router;
