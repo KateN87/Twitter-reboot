7e64330547317a18f4251284c7359f345826d1a8
aylinataei <114648016+aylinataei@users.noreply.github.com>
2023-04-18 13:27:36 +0200

    login

--- backend/src/main.js
+++ backend/src/main.js
@@ -6,19 +6,17 @@ import logRoutes from "./Routes/logRoutes.js";
 
 import cors from 'cors'
 
+const app = express();
 app.use(cors({
     origin: "http://localhost:3000"
 }))
-
-const app = express();
-
 
 app.use(express.json());
 
 app.use("/log", logRoutes);
 
 app.get("/users", (req, res) => {
-	res.send(db.data.users);
+    res.send(db.data.users);
 });
 
 export { app };
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch('http://localhost:3001/login', {
+    const response = await fetch('http://localhost:3000/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
