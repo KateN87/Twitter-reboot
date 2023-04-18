2d68b090dc5946599e0fbc82ac87011310e28ad9
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-04-18 16:29:27 +0200

    imports fixed(header, viwetweets)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,19 @@
-import {Header} from './Header.js'
-import { ViewTweet } from './ViewTweets.js'
+import { Header } from '../components/Header'
+import { ViewTweet } from '../components/ViewTweets'
 import { useState, useEffect } from 'react';
 
 export default function Home() {
-  const [fetchedTweets, setFetchedTweets] = useState([]);
+   const [fetchedTweets, setFetchedTweets] = useState([]);
    useEffect(() => {
 
-        const fetchTweets = async () => {
-            const response = await fetch('http://localhost:3001/tweets');
-            const tweets = await response.json();
-            setFetchedTweets(tweets)
-        }
-        fetchTweets()
+      const fetchTweets = async () => {
+         const response = await fetch('http://localhost:3001/tweets');
+         const tweets = await response.json();
+         setFetchedTweets(tweets)
+      }
+      fetchTweets()
    })
-   
+
 
    return (
       <div>
