c6604be85e48c75cf922401266b4b379f4d10dc3
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-19 19:20:05 +0200

    fixed signup

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,100 +1,111 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 //TODO
 //1. Logga in användaren efter ok response
 
 const Signup = () => {
-	const [error, setError] = useState(null);
-	const dispatch = useDispatch();
+    const [error, setError] = useState(null);
+    const dispatch = useDispatch();
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setError(null);
-		const newUser = {
-			username: `@${e.target.username.value}`,
-			password: e.target.password.value,
-			verifyPass: e.target.verifyPass.value,
-			email: e.target.email.value,
-			nickname: e.target.nickname.value,
-			about: e.target.about.value,
-			occupation: e.target.occupation.value,
-			hometown: e.target.hometown.value,
-			website: e.target.website.value,
-		};
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError(null);
+        const {
+            username,
+            password,
+            verifyPass,
+            email,
+            nickname,
+            about,
+            occupation,
+            hometown,
+            website,
+        } = e.target;
+        const newUser = {
+            username: `@${username.value}`,
+            password: password.value,
+            verifyPass: verifyPass.value,
+            email: email.value,
+            nickname: nickname.value,
+            about: about.value,
+            occupation: occupation.value,
+            hometown: hometown.value,
+            website: website.value,
+        };
 
-		const response = await fetch("http://localhost:3001/log/signup", {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify(newUser),
-		});
+        const response = await fetch('http://localhost:3001/log/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newUser),
+        });
 
-		const json = await response.json();
+        const json = await response.json();
 
-		if (!response.ok) {
-			//sätter error till felmeddelandet från backend
-			setError(json.error);
-		}
-		if (response.ok) {
-			//Sätter localstorage, innehåller username och token
-			localStorage.setItem("user", JSON.stringify(json));
+        if (!response.ok) {
+            //sätter error till felmeddelandet från backend
+            setError(json.error);
+        }
+        if (response.ok) {
+            //Sätter localstorage, innehåller username och token
+            localStorage.setItem('user', JSON.stringify(json));
 
-			//Logga in användaren
-			dispatch({ type: "LOGIN_USER", payload: json });
+            //Logga in användaren
+            dispatch({ type: 'LOGIN_USER', payload: json });
 
-			//nollställer formuläret
-			e.target.reset();
-		}
-	};
+            //nollställer formuläret
+            e.target.reset();
+        }
+    };
 
-	return (
-		<div className='login-container'>
-			<form className='login-form' onSubmit={handleSubmit}>
-				<h1>Sign up</h1>
-				<label htmlFor='username'>Username:</label>
-				<input type='text' id='username' />
+    return (
+        <div className='login-container'>
+            <form className='login-form' onSubmit={handleSubmit}>
+                <h1>Sign up</h1>
+                <label htmlFor='username'>Username:</label>
+                <input type='text' id='username' />
 
-				<br />
-				<label htmlFor='password'>Password:</label>
-				<input type='password' id='password' />
+                <br />
+                <label htmlFor='password'>Password:</label>
+                <input type='password' id='password' />
 
-				<br />
+                <br />
 
-				<label htmlFor='verifyPass'>Verify password:</label>
-				<input type='password' id='verifyPass' />
+                <label htmlFor='verifyPass'>Verify password:</label>
+                <input type='password' id='verifyPass' />
 
-				<br />
-				<label htmlFor='email'>Email:</label>
-				<input type='text' id='email' />
+                <br />
+                <label htmlFor='email'>Email:</label>
+                <input type='text' id='email' />
 
-				<br />
-				<label htmlFor='nickname'>Nickname:</label>
-				<input type='text' id='nickname' />
+                <br />
+                <label htmlFor='nickname'>Nickname:</label>
+                <input type='text' id='nickname' />
 
-				<br />
-				<label htmlFor='about'>About:</label>
-				<input type='text' id='about' />
+                <br />
+                <label htmlFor='about'>About:</label>
+                <input type='text' id='about' />
 
-				<br />
-				<label htmlFor='occupation'>Occupation:</label>
-				<input type='text' id='occupation' />
+                <br />
+                <label htmlFor='occupation'>Occupation:</label>
+                <input type='text' id='occupation' />
 
-				<br />
-				<label htmlFor='hometown'>Hometown:</label>
-				<input type='text' id='hometown' />
+                <br />
+                <label htmlFor='hometown'>Hometown:</label>
+                <input type='text' id='hometown' />
 
-				<br />
-				<label htmlFor='website'>Website:</label>
-				<input type='text' id='website' />
+                <br />
+                <label htmlFor='website'>Website:</label>
+                <input type='text' id='website' />
 
-				<br />
-				<button type='submit'>Sign up</button>
+                <br />
+                <button type='submit'>Sign up</button>
 
-				{/* Om error INTE är null visas denna*/}
-				{error && <div className='error'>{error}</div>}
-			</form>
-		</div>
-	);
+                {/* Om error INTE är null visas denna*/}
+                {error && <div className='error'>{error}</div>}
+            </form>
+        </div>
+    );
 };
 
 export default Signup;
