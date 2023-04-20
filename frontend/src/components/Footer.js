d12c924711e4c6e2100e53c12b90a3c9985db0d4
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-20 20:20:23 +0200

    logout started

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { HiDotsHorizontal } from 'react-icons/hi';
 
@@ -8,7 +8,13 @@ import { useState } from 'react';
 const Footer = () => {
     const user = useSelector((state) => state.userReducer.user);
     const [active, setActive] = useState(false);
+    const dispatch = useDispatch();
 
+    const handleLogout = () => {
+        setActive(false);
+        localStorage.removeItem('user');
+        dispatch({ type: 'LOGOUT', payload: null });
+    };
     return (
         <footer>
             {!user && (
@@ -25,14 +31,23 @@ const Footer = () => {
                     </div>
                 </>
             )}
-            <div className='userinfo'>
-                <img src={user.avatar} />
-                <div className='names'>
-                    <h2>{user.nickname}</h2>
-                    <h3>{user.username}</h3>
+            {user && (
+                <div className='userinfo' onClick={() => setActive(true)}>
+                    <img src={user.avatar} />
+                    <div className='names'>
+                        <h2>{user.nickname}</h2>
+                        <h3>{user.username}</h3>
+                    </div>
+                    <HiDotsHorizontal className='threedots' />
                 </div>
-                <HiDotsHorizontal className='threedots' />
-            </div>
+            )}
+            {active && (
+                <div className='logout-box'>
+                    <h1>Log out {user.nickname}?</h1>
+                    <button onClick={handleLogout}>Log out</button>
+                    <button onClick={() => setActive(false)}>Cancel</button>
+                </div>
+            )}
         </footer>
     );
 };
