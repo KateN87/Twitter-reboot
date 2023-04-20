27026e297285c3f37cd978a6f1ddb9a450bdbcc7
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-20 19:56:39 +0200

    changed footer when logged in

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,28 +1,40 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { HiDotsHorizontal } from 'react-icons/hi';
+
+import '../footer.css';
+import { useState } from 'react';
 
 const Footer = () => {
-	const user = useSelector((state) => state.userReducer.user);
+    const user = useSelector((state) => state.userReducer.user);
+    const [active, setActive] = useState(false);
 
-	if (user === null) {
-		return <div>Loading...</div>;
-	}
-
-	return (
-		<footer>
-			<h2>Missa inte vad som händer</h2>
-			<h4>Folk på twitter får reda på allt först.</h4>
-			<div id='buttons'>
-				<Link to='/login'>
-					<button>Logga in</button>
-				</Link>
-				<Link to='/signup'>
-					<button id='createuser'>Skapa användare</button>
-				</Link>
-				<button id='Logout'>Log out</button>
-			</div>
-		</footer>
-	);
+    return (
+        <footer>
+            {!user && (
+                <>
+                    <h2>Missa inte vad som händer</h2>
+                    <h4>Folk på twitter får reda på allt först.</h4>
+                    <div id='buttons'>
+                        <Link to='/login'>
+                            <button>Logga in</button>
+                        </Link>
+                        <Link to='/signup'>
+                            <button id='createuser'>Skapa användare</button>
+                        </Link>
+                    </div>
+                </>
+            )}
+            <div className='userinfo'>
+                <img src={user.avatar} />
+                <div className='names'>
+                    <h2>{user.nickname}</h2>
+                    <h3>{user.username}</h3>
+                </div>
+                <HiDotsHorizontal className='threedots' />
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
