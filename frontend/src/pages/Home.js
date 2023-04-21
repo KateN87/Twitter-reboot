d99f19d4a06a90e691f09fd580eaf6b77e4d0c2f
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-04-21 10:54:44 +0200

    added createTweet on home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,33 +3,35 @@ import { ViewTweet } from '../components/ViewTweets';
 import { useState, useEffect } from 'react';
 import { Searchbar } from '../icons/Searchbar';
 import { RegisterLoginDialogue } from '../components/RegisterLoginDialogue';
+import CreateTweet from '../components/CreateTweet';
 
 export default function Home() {
-    const [fetchedTweets, setFetchedTweets] = useState([]);
-    useEffect(() => {
-        const fetchTweets = async () => {
-            const response = await fetch('http://localhost:3001/tweets');
-            const tweets = await response.json();
+   const [fetchedTweets, setFetchedTweets] = useState([]);
+   useEffect(() => {
+      const fetchTweets = async () => {
+         const response = await fetch('http://localhost:3001/tweets');
+         const tweets = await response.json();
 
-            tweets.sort(function (a, b) {
-                var c = new Date(a.timestamp);
-                var d = new Date(b.timestamp);
-                return d - c;
-            });
-            setFetchedTweets(tweets);
-        };
-        fetchTweets();
-    }, []);
+         tweets.sort(function (a, b) {
+            var c = new Date(a.timestamp);
+            var d = new Date(b.timestamp);
+            return d - c;
+         });
+         setFetchedTweets(tweets);
+      };
+      fetchTweets();
+   }, []);
 
-    return (
-        <div>
-            <Header />
-            <Searchbar />
-            <RegisterLoginDialogue />
-            <ViewTweet
-                fetchedTweets={fetchedTweets}
-                setFetchedTweets={setFetchedTweets}
-            />
-        </div>
-    );
+   return (
+      <div>
+         <Header />
+         <Searchbar />
+         <CreateTweet />
+         <RegisterLoginDialogue />
+         <ViewTweet
+            fetchedTweets={fetchedTweets}
+            setFetchedTweets={setFetchedTweets}
+         />
+      </div>
+   );
 }
