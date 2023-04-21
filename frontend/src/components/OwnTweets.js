3d767e16de434537318b3ea879db9d94a522c3c0
phatasss <lovisaaryd@gmail.com>
2023-04-21 12:01:46 +0200

    Fixade tiden på tweets

--- frontend/src/components/OwnTweets.js
+++ frontend/src/components/OwnTweets.js
@@ -20,20 +20,26 @@ export const OwnTweets = () => {
 	}, []);
 
 	const returntimestamp = (tweet) => {
-		let timestamp = formatDistanceToNow(new Date(tweet.timestamp));
-		let time = timestamp.split(" ");
-		if (time[1] !== "day") {
-			let remove = time.indexOf("about");
-			let firstremove = time.splice(remove, 1);
-			let remove2 = time.indexOf("hours");
-			let secondremove = time.splice(remove2, 1);
-			console.log(time);
-			let realtime = `${time + "h"}`;
-			return realtime;
-		} else {
-			let realtime = `${time[0] + " " + time[1]}`;
-			return realtime;
-		}
+            let timestamp = formatDistanceToNow(new Date(tweet.timestamp));
+            let time = timestamp.split(' ');
+            console.log(time)
+            if (time[0] === "about") {
+               let remove = time.indexOf('about');
+               let firstremove = time.splice(remove, 1);
+               let remove2 = time.indexOf('hours');
+               let secondremove = time.splice(remove2, 1);
+               let realtime = `${time + ' h'}`;
+               return realtime;
+            } else if(time[1] === "days") {
+               let realtime = `${time[0] + ' d'}`;
+               return realtime;
+            } else if(time[1] === "minutes"){
+              let realtime = `${time[0] + " m"}`
+              return realtime;
+            } else {
+              let realtime = `${time[0] + " s"}`
+              return realtime;
+            }
 	};
 
 	return (
