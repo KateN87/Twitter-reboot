7c244342f2f6c2daef2d8bb93bf4a6fca2ab7697
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-21 17:50:30 +0200

    fixed bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,58 +1,60 @@
-import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import { Header } from "./components/Header";
-import Footer from "./components/Footer";
-
+import Login from './components/Login';
+import Signup from './components/Signup';
+import { Header } from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
-	const dispatch = useDispatch();
-	const user = useSelector((state) => state.userReducer.user);
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.userReducer.user);
 
-	useEffect(() => {
-		const checkUser = JSON.parse(localStorage.getItem("user"));
+    useEffect(() => {
+        const checkUser = JSON.parse(localStorage.getItem('user'));
 
-		if (checkUser) {
-			const checkJwt = async () => {
-				const response = await fetch("http://localhost:3001/locked/test", {
-					headers: {
-						Authorization: `Bearer ${checkUser.token}`,
-						"Content-Type": "application/json",
-					},
-				});
+        if (checkUser) {
+            const checkJwt = async () => {
+                const response = await fetch(
+                    'http://localhost:3001/locked/test',
+                    {
+                        headers: {
+                            Authorization: `Bearer ${checkUser.token}`,
+                            'Content-Type': 'application/json',
+                        },
+                    }
+                );
 
-				if (response.ok) {
-					const loggedUser = await response.json();
+                if (response.ok) {
+                    const loggedUser = await response.json();
 
-					dispatch({ type: "LOGIN_USER", payload: loggedUser });
-				}
-			};
-			checkJwt();
-		}
-	}, []);
+                    dispatch({ type: 'LOGIN_USER', payload: loggedUser });
+                }
+            };
+            checkJwt();
+        }
+    }, []);
 
-	return (
-		<div className='App'>
-			<Header />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route
-					path='/profile'
-					element={user ? <Profile /> : <Navigate to='/' />}
-				/>
-				<Route path='/login' element={<Login />} />
-				<Route path='/signup' element={<Signup />} />
-			</Routes>
-			<Footer />
-		</div>
-	);
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route
+                    path='/profile'
+                    element={user ? <Profile /> : <Navigate to='/' />}
+                />
+                <Route path='/login' element={<Login />} />
+                <Route path='/signup' element={<Signup />} />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
