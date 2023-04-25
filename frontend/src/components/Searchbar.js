a7f8649e979a11d91a28b54e7f2f1234dd84335c
aylinataei <114648016+aylinataei@users.noreply.github.com>
2023-04-25 15:10:28 +0200

    seachbar

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 export const Searchbar = () => {
-    return(
-        <form id="search">
-        <input type="text" placeholder='Sök på twitter' id="searchbar"></input>
-        <div id="tabs">
-            <p className='trending'>För dig</p>
-            <p className='trending'>Trendar</p>
-        </div>
+    const [users, setUsers] = useState([]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const searchQuery = event.target.elements.searchbar.value;
+        try {
+            const response = await fetch('http://localhost:3001/users');
+            const data = await response.json();
+            const matchingUsers = data.filter((user) =>
+                user.username.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+            setUsers(matchingUsers);
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
+    };
+
+    return (
+
+        <form id="search " onSubmit={handleSubmit}>
+            <input type="text" placeholder="Sök på twitter" name="searchbar" />
+            <button type="submit">Sök</button>
+
+            <div id="tabs">
+                <p className='trending'>För dig</p>
+                <p className='trending'>Trendar</p>
+            </div>
+            {users.length > 0 && (
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>{user.username}</li>
+                    ))}
+                </ul>
+            )}
         </form>
-    )
+    );
+
 }
