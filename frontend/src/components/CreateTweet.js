cf65b182d29396065fa84470dfa6461352d364e7
Kate <110845948+KateN87@users.noreply.github.com>
2023-04-28 10:49:04 +0200

    fixed create new tweet

--- frontend/src/components/CreateTweet.js
+++ frontend/src/components/CreateTweet.js
@@ -1,61 +1,64 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 export default function CreateTweet() {
-   const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-   async function submitTweet(event) {
-      event.preventDefault();
+	async function submitTweet(event) {
+		event.preventDefault();
 
-      const textInput = event.target.tweet.value;
-      const checkUser = JSON.parse(localStorage.getItem('user'));
-      const wordsArray = textInput.split(/[\s\n]+/);
-      const foundHashtag = wordsArray.filter((word) => word.startsWith('#'));
+		const textInput = event.target.tweet.value;
+		const checkUser = JSON.parse(localStorage.getItem("user"));
+		const wordsArray = textInput.split(/[\s\n]+/);
+		const foundHashtag = wordsArray.filter((word) => word.startsWith("#"));
 
-      if (!checkUser) {
-         console.log('User not authenticated');
-         return;
-      }
+		if (!checkUser) {
+			console.log("User not authenticated");
+			return;
+		}
 
-      const newTweetReq = {
-         tweet: textInput,
-         username: checkUser.username,
-         hashtags: foundHashtag,
-      };
+		const newTweetReq = {
+			tweet: textInput,
+			username: checkUser.username,
+			hashtags: foundHashtag,
+		};
 
-      const options = {
-         method: 'POST',
-         body: JSON.stringify(newTweetReq),
-         headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${checkUser.token}`,
-         },
-      };
+		const options = {
+			method: "POST",
+			body: JSON.stringify(newTweetReq),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${checkUser.token}`,
+			},
+		};
 
-      try {
-         const response = await fetch('http://localhost:3001/locked/tweets', options);
-         if (!response.ok) {
-            throw new Error('Failed to send tweet');
-         }
-         const newTweet = await response.json();
-         setNewTweet(newTweet)
-         dispatch({ type: 'SEND_TWEET', payload: newTweet });
-      } catch (error) {
-         console.error(error);
-      }
-   }
+		try {
+			const response = await fetch(
+				"http://localhost:3001/locked/tweets",
+				options
+			);
+			if (!response.ok) {
+				throw new Error("Failed to send tweet");
+			}
+			const newTweet = await response.json();
 
-   return (
-      <div className='tweet-component'>
-         <form onSubmit={submitTweet} className='tweet-form' action=''>
-            <textarea
-               id='tweet'
-               name='tweet'
-               rows='5'
-               maxLength='140'
-               placeholder='Write tweet...'
-            ></textarea>
-            <button type='submit'>Tweet</button>
-         </form>
-      </div>
-   );
+			dispatch({ type: "SEND_TWEET", payload: newTweet });
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return (
+		<div className='tweet-component'>
+			<form onSubmit={submitTweet} className='tweet-form' action=''>
+				<textarea
+					id='tweet'
+					name='tweet'
+					rows='5'
+					maxLength='140'
+					placeholder='Write tweet...'
+				></textarea>
+				<button type='submit'>Tweet</button>
+			</form>
+		</div>
+	);
 }
