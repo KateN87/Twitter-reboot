c8fe2591dcb83d717fcf9be2249e813b35a05323
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-28 18:24:25 +0200

    moved searchbar to right side

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,19 @@
-import { Header } from "../components/Header";
-import { ViewTweet } from "../components/ViewTweets";
-import { useState, useEffect } from "react";
-import { Searchbar } from "../components/Searchbar";
-import { RegisterLoginDialogue } from "../components/RegisterLoginDialogue";
-import CreateTweet from "../components/CreateTweet";
-import { useDispatch, useSelector } from "react-redux";
+import { Header } from '../components/Header';
+import { ViewTweet } from '../components/ViewTweets';
+import { useState, useEffect } from 'react';
+import { Searchbar } from '../components/Searchbar';
+import { RegisterLoginDialogue } from '../components/RegisterLoginDialogue';
+import CreateTweet from '../components/CreateTweet';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Home({ setId, id }) {
     const dispatch = useDispatch();
-    const user = JSON.parse(localStorage.getItem("user"));
+    const user = JSON.parse(localStorage.getItem('user'));
     // Lägg till isloading för att vänta på user
 
     useEffect(() => {
         const fetchTweets = async () => {
-            const response = await fetch("http://localhost:3001/tweets");
+            const response = await fetch('http://localhost:3001/tweets');
             const tweets = await response.json();
 
             tweets.sort(function (a, b) {
@@ -21,19 +21,19 @@ export default function Home({ setId, id }) {
                 var d = new Date(b.timestamp);
                 return d - c;
             });
-            dispatch({ type: "GET_TWEETS", payload: tweets });
+            dispatch({ type: 'GET_TWEETS', payload: tweets });
         };
         fetchTweets();
     }, []);
     return (
         <>
             <div className='middle-main-container'>
-                <Searchbar />
                 {user && <CreateTweet />}
 
                 <ViewTweet setId={setId} id={id} />
             </div>
             <div className='right-main-container'>
+                <Searchbar />
                 {!user && <RegisterLoginDialogue />}
             </div>
         </>
