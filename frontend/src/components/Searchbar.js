4b174284b45c8b77bfa0231f8ba7e514266f8915
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-04-28 22:33:05 +0200

    matching hashtags in tweets seem to workish

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -3,113 +3,89 @@ import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Searchbar = () => {
-	const [hashtags, setHashtags] = useState([]);
-	const [matchingHashtags, setMatchingHashtags] = useState([]);
-	const [matchingTweets, setMatchingTweets] = useState([]);
+   const [users, setUsers] = useState([]);
+   const [errorMessage, setErrorMessage] = useState(null);
+   const [hashtags, setHashtags] = useState([]);
+   const fetchedTweets = useSelector((state) => state.tweetReducer);
 
-	const [searchInput, setSearchInput] = useState("");
-	const [users, setUsers] = useState([]);
-	const [errorMessage, setErrorMessage] = useState(null);
-	const fetchedTweets = useSelector((state) => state.tweetReducer) || [];
+   const fetchMatchingUsers = async (searchQuery) => {
+      try {
+         const response = await fetch("http://localhost:3001/users");
+         const data = await response.json();
+         const matchingUsers = data.filter((user) =>
+            user.username.toLowerCase().includes(searchQuery.toLowerCase())
+         );
+         return matchingUsers;
+      } catch (error) {
+         console.error(error);
+         setErrorMessage("Failed to fetch users.");
+         return [];
+      }
+   };
 
-	const fetchMatchingUsers = async (searchQuery) => {
-		try {
-			const response = await fetch("http://localhost:3001/users");
-			const data = await response.json();
-			const matchingUsers = data.filter((user) =>
-				user.username.toLowerCase().includes(searchQuery.toLowerCase())
-			);
-			return matchingUsers;
-		} catch (error) {
-			console.error(error);
-			setErrorMessage("Failed to fetch users.");
-			return [];
-		}
-	};
 
-	const handleSearchInputChange = (event) => {
-		const input = event.target.value;
-		setSearchInput(input);
+   /*    useEffect(() => {
+         const fetchHashtags = async () => {
+            try {
+               const response = await fetch("http://localhost:3001/hashtags");
+               const data = await response.json();
+               setHashtags(data);
+            } catch (error) {
+               console.error(error);
+               setErrorMessage("Failed to fetch hashtags.");
+            }
+         };
+         fetchHashtags();
+      }, []); */
 
-		/* fetchedTweets.map((tweet) =>
-			console.log("Hashtags in every tweet", tweet.hashtags)
-		); */
-		if (searchInput !== "") {
-			const filteredTweets = fetchedTweets.filter((tweet) =>
-				tweet.hashtags.includes(searchInput)
-			);
-			console.log("FETCHEDTWEETS", filteredTweets);
-		}
 
-		//console.log('filtered tweets: ', filteredTweets)
 
-		//console.log('searchinput: ', searchInput);
+   const handleSubmit = async (event) => {
+      event.preventDefault();
+      const searchQuery = event.target.elements.searchbar.value;
 
-		/*       const input = event.target.value;
-            setSearchInput(input);
-            const filteredHashtags = hashtags.filter((hashtag) =>
-               hashtag.includes(input)
-            );
-            setHashtags(...filteredHashtags);
-            const filteredTweets = fetchedTweets.filter((tweet) =>
-               tweet.hashtags.some((ht) => filteredHashtags.includes(ht))
-            );
-      
-            setMatchingTweets(filteredTweets); */
-	};
+      // Fetch matching hashtags
+      const matchingHashtags = fetchedTweets.filter((tweet) =>
+         tweet.hashtags.includes(searchQuery.toLowerCase())
+      );
+      console.log('aaaaaaa', matchingHashtags)
 
-	useEffect(() => {
-		const fetchHashtags = async () => {
-			try {
-				const response = await fetch("http://localhost:3001/hashtags");
-				const data = await response.json();
-				setHashtags(data);
-			} catch (error) {
-				console.error(error);
-				setErrorMessage("Failed to fetch hashtags.");
-			}
-		};
-		fetchHashtags();
-	}, []);
+      const matchingUsers = await fetchMatchingUsers(searchQuery);
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		const searchQuery = event.target.elements.searchbar.value;
-		const matchingUsers = await fetchMatchingUsers(searchQuery);
-		if (matchingUsers.length === 0) {
-			setErrorMessage(`No user found with the name ${searchQuery}`);
-			setUsers([]);
-		} else {
-			setUsers(matchingUsers);
-		}
-	};
+      if (matchingUsers.length === 0 && matchingHashtags.length === 0) {
+         setErrorMessage(`No user or hashtag found with the name ${searchQuery}`);
+         setUsers([]);
+      } else {
+         setUsers(matchingUsers);
+      }
+   };
 
-	return (
-		<div>
-			<form id='submit' onSubmit={handleSubmit}>
-				<input
-					onChange={handleSearchInputChange}
-					value={searchInput}
-					type='text'
-					placeholder='Search on Twitter'
-					id='searchbar'
-				></input>
-				<button type='submit'>Search</button>
-			</form>
-			<ul>
-				{users.map((user) => (
-					<div key={user.id}>
-						<p>
-							<Link className='link' to={`/profile/${user.id}`}>
-								{user.username}
-							</Link>
-						</p>
-					</div>
-				))}
-			</ul>
-			{errorMessage && <p>{errorMessage}</p>}
-		</div>
-	);
+   return (
+      <div>
+         <form id='submit' onSubmit={handleSubmit}>
+            <input
+               /*                onChange={handleSearchInputChange}
+                */              /*  value={searchInput} */
+               type='text'
+               placeholder='Search on Twitter'
+               id='searchbar'
+            ></input>
+            <button type='submit'>Search</button>
+         </form>
+         <ul>
+            {users.map((user) => (
+               <div key={user.id}>
+                  <p>
+                     <Link className='link' to={`/profile/${user.id}`}>
+                        {user.username}
+                     </Link>
+                  </p>
+               </div>
+            ))}
+         </ul>
+         {errorMessage && <p>{errorMessage}</p>}
+      </div>
+   );
 };
 
 export { Searchbar };
