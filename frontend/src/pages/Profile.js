88c5ab4da83e0cfd5fa7807dcc1c3a8366ca5263
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-28 23:40:32 +0200

    started refaktoring

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,19 +1,20 @@
-import { default as ProfileInformation } from "../components/ProfileInformation.js";
+import { default as ProfileInformation } from '../components/ProfileInformation.js';
 
-import { OwnTweets } from "../components/OwnTweets";
-import CreateTweet from "../components/CreateTweet.js";
+import { OwnTweets } from '../components/OwnTweets';
+import CreateTweet from '../components/CreateTweet.js';
 
-export default function Profile({ id, setId }) {
-	let idparam;
-	return (
-		<>
-			<div className='middle-main-container'>
-				<CreateTweet />
-				<OwnTweets id={id} idparam={idparam}></OwnTweets>
-			</div>
-			<div className='right-main-container'>
-				<ProfileInformation id={id} setId={setId} idparam={idparam} />
-			</div>
-		</>
-	);
+export default function Profile(/* { id, setId } */) {
+    /* let idparam; */
+    return (
+        <>
+            <div className='middle-main-container'>
+                <CreateTweet />
+                <OwnTweets /* id={id} idparam={idparam} */></OwnTweets>
+            </div>
+            <div className='right-main-container'>
+                <ProfileInformation /* id={id} setId={setId} idparam={idparam} */
+                />
+            </div>
+        </>
+    );
 }
