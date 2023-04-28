c00e671cc3280733df4018c18e325f5b47c06364
Kate Nordqvist <kate.nordqvist@gmail.com>
2023-04-28 20:33:14 +0200

    Fixat so sidan rerenders när man skickar ny tweet

--- frontend/src/components/OwnTweets.js
+++ frontend/src/components/OwnTweets.js
@@ -1,27 +1,19 @@
 import { useEffect, useState } from 'react';
-import { formatDistanceToNow } from 'date-fns';
 import { useParams } from 'react-router-dom';
+import returntimestamp from '../formatTimestamp';
+import { useSelector } from 'react-redux';
 
-export const OwnTweets = ({id, idparam}) => {
+export const OwnTweets = () => {
     const [ownTweets, setOwnTweets] = useState([]);
-    idparam = useParams().id
-    useEffect(() => {
+    const tweetsList = useSelector((state) => state.tweetReducer);
+    const idparam = useParams().id;
 
+    useEffect(() => {
         const fetchOwnTweets = async () => {
-            let username
-            if(id === 0){
-                const response = await fetch('http://localhost:3001/users/' + idparam)
-                const user = await response.json()
-                username = user.username
-            } else {
-                const response = await fetch('http://localhost:3001/users/' + id)
-            const user = await response.json()
-            username = user.username
-            }
-            const response2 = await fetch(
-                'http://localhost:3001/tweets/' + username
+            const response = await fetch(
+                'http://localhost:3001/tweets/' + idparam
             );
-            const tweets = await response2.json();
+            const tweets = await response.json();
             tweets.sort(function (a, b) {
                 var c = new Date(a.timestamp);
                 var d = new Date(b.timestamp);
@@ -30,29 +22,12 @@ export const OwnTweets = ({id, idparam}) => {
             setOwnTweets(tweets);
         };
         fetchOwnTweets();
-    }, []);
+    }, [tweetsList]);
 
-    const returntimestamp = (tweet) => {
-        let timestamp = formatDistanceToNow(new Date(tweet.timestamp));
-        let time = timestamp.split(' ');
-        if (time[0] === 'about') {
-            let remove = time.indexOf('about');
-            let firstremove = time.splice(remove, 1);
-            let remove2 = time.indexOf('hours');
-            let secondremove = time.splice(remove2, 1);
-            let realtime = `${time + ' h'}`;
-            return realtime;
-        } else if (time[1] === 'days' || time[1] === 'day') {
-            let realtime = `${time[0] + ' d'}`;
-            return realtime;
-        } else if (time[1] === 'minutes') {
-            let realtime = `${time[0] + ' m'}`;
-            return realtime;
-        } else {
-            let realtime = `${time[0] + ' s'}`;
-            return realtime;
-        }
-    };
+    if (idparam === undefined) {
+        console.log('TEST');
+        return <div>Loading...</div>;
+    }
 
     return (
         <div id='tweet-big-container'>
@@ -73,3 +48,14 @@ export const OwnTweets = ({id, idparam}) => {
         </div>
     );
 };
+
+/*             let username
+            if(id === 0){
+                const response = await fetch('http://localhost:3001/users/' + idparam)
+                const user = await response.json()
+                username = user.username
+            } else {
+                const response = await fetch('http://localhost:3001/users/' + id)
+            const user = await response.json()
+            username = user.username
+            } */
