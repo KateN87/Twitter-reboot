df558d1741d616281a525e74dc063411f9002712
Kate <110845948+KateN87@users.noreply.github.com>
2023-04-28 09:31:22 +0200

    Added createTweet and removed tabs from searchbar

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,17 +1,19 @@
-import { default as ProfileInformation } from '../components/ProfileInformation.js';
+import { default as ProfileInformation } from "../components/ProfileInformation.js";
 
-import { OwnTweets } from '../components/OwnTweets';
+import { OwnTweets } from "../components/OwnTweets";
+import CreateTweet from "../components/CreateTweet.js";
 
 export default function Profile({ id, setId }) {
-    let idparam
-    return (
-        <>
-            <div className='middle-main-container'>
-                <OwnTweets id={id} idparam={idparam}></OwnTweets>
-            </div>
-            <div className='right-main-container'>
-                <ProfileInformation id={id} setId={setId} idparam={idparam}/>
-            </div>
-        </>
-    );
+	let idparam;
+	return (
+		<>
+			<div className='middle-main-container'>
+				<CreateTweet />
+				<OwnTweets id={id} idparam={idparam}></OwnTweets>
+			</div>
+			<div className='right-main-container'>
+				<ProfileInformation id={id} setId={setId} idparam={idparam} />
+			</div>
+		</>
+	);
 }
