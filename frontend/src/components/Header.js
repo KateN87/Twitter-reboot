24e18c8498986e89d33e13ce6817a91a1d01daa8
Kate <110845948+KateN87@users.noreply.github.com>
2023-05-02 11:18:34 +0200

    fixed bug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,34 @@
-import { useSelector } from 'react-redux';
-import '../styles/Header.css';
+import { useSelector } from "react-redux";
+import "../styles/Header.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import logo from '../icons/twitter-logo-.png';
-import hashtag from '../icons/hashtagtwitter.png';
-import MiniUserInfo from './MiniUserInfo.js';
-import Trending from './Trending';
+import logo from "../icons/twitter-logo-.png";
+import hashtag from "../icons/hashtagtwitter.png";
+import MiniUserInfo from "./MiniUserInfo.js";
+import Trending from "./Trending";
 
 export const Header = () => {
-    const user = useSelector((state) => state.userReducer);
+	const user = useSelector((state) => state.userReducer);
+	console.log(user);
+	return (
+		<nav>
+			<img src={logo} id='logga'></img>
+			<img src={hashtag} className='hashtag'></img>
+			<h3 className='hashtag' id='explore'>
+				Trending hashtags
+			</h3>
+			<Trending />
+			<Link to={`/`}>
+				<button>Home</button>
+			</Link>
+			{user !== null && (
+				<Link to={`/profile/${user.id}`}>
+					<button>My profile</button>
+				</Link>
+			)}
 
-    return (
-        <nav>
-            <img src={logo} id='logga'></img>
-            <img src={hashtag} className='hashtag'></img>
-            <h3 className='hashtag' id='explore'>
-                Trending hashtags
-            </h3>
-            <Trending />
-            <Link to={`/`}>
-                <button>Home</button>
-            </Link>
-            {user !== undefined && (
-                <Link to={`/profile/${user.id}`}>
-                    <button>My profile</button>
-                </Link>
-            )}
-
-            {user !== undefined && <MiniUserInfo />}
-        </nav>
-    );
+			{user !== null && <MiniUserInfo />}
+		</nav>
+	);
 };
