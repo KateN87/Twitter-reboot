d019878781db47aab6753cc4ae85f48d725d0fe0
Kate <110845948+KateN87@users.noreply.github.com>
2023-05-03 13:16:23 +0200

    import showtweets

--- frontend/src/components/OwnTweets.js
+++ frontend/src/components/OwnTweets.js
@@ -1,32 +1,30 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ShowTweetsContainer from './showTweetsContainer';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ShowTweetsContainer from "./showTweetsContainer.js";
 
 export const OwnTweets = () => {
-    const [tweetsList, setTweetsList] = useState([]);
-    const allTweets = useSelector((state) => state.tweetReducer);
-    const idparam = useParams().id;
+	const [tweetsList, setTweetsList] = useState([]);
+	const allTweets = useSelector((state) => state.tweetReducer);
+	const idparam = useParams().id;
 
-    useEffect(() => {
-        const fetchOwnTweets = async () => {
-            const response = await fetch(
-                'http://localhost:3001/tweets/' + idparam
-            );
-            const tweets = await response.json();
-            tweets.sort(function (a, b) {
-                var c = new Date(a.timestamp);
-                var d = new Date(b.timestamp);
-                return d - c;
-            });
-            setTweetsList(tweets);
-        };
-        fetchOwnTweets();
-    }, [idparam, allTweets]);
+	useEffect(() => {
+		const fetchOwnTweets = async () => {
+			const response = await fetch("http://localhost:3001/tweets/" + idparam);
+			const tweets = await response.json();
+			tweets.sort(function (a, b) {
+				var c = new Date(a.timestamp);
+				var d = new Date(b.timestamp);
+				return d - c;
+			});
+			setTweetsList(tweets);
+		};
+		fetchOwnTweets();
+	}, [idparam, allTweets]);
 
-    if (idparam === undefined) {
-        console.log('TEST');
-        return <div>Loading...</div>;
-    }
-    return <ShowTweetsContainer tweetsList={tweetsList} />;
+	if (idparam === undefined) {
+		console.log("TEST");
+		return <div>Loading...</div>;
+	}
+	return <ShowTweetsContainer tweetsList={tweetsList} />;
 };
