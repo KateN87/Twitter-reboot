1d070cd95249773602f9d84fcc2942062f3c6043
Kate <110845948+KateN87@users.noreply.github.com>
2023-05-03 14:12:05 +0200

    Fixed ownTweets

--- frontend/src/components/OwnTweets.js
+++ frontend/src/components/OwnTweets.js
@@ -1,52 +1,32 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ShowTweetsContainer from './showTweetsContainer';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ShowTweetsContainer from "./showTweetsContainer";
 
 export const OwnTweets = () => {
-   const [tweetsList, setTweetsList] = useState([]);
-   const allTweets = useSelector((state) => state.tweetReducer);
-   const idparam = useParams().id;
+	const [tweetsList, setTweetsList] = useState([]);
+	const idparam = useParams().id;
 
-   useEffect(() => {
-      const fetchOwnTweets = async () => {
-         const response = await fetch(
-            'http://localhost:3001/tweets/' + idparam
-         );
-         const tweets = await response.json();
-         tweets.sort(function (a, b) {
-            var c = new Date(a.timestamp);
-            var d = new Date(b.timestamp);
-            return d - c;
-         });
-         setOwnTweets(tweets);
-      };
-      fetchOwnTweets();
-   }, [idparam, tweetsList]);
+	useEffect(() => {
+		const fetchOwnTweets = async () => {
+			const response = await fetch("http://localhost:3001/tweets/" + idparam);
+			const tweets = await response.json();
+			tweets.sort(function (a, b) {
+				var c = new Date(a.timestamp);
+				var d = new Date(b.timestamp);
+				return d - c;
+			});
+			setTweetsList(tweets);
+		};
+		fetchOwnTweets();
+	}, [idparam, tweetsList]);
 
-   if (idparam === undefined) {
-      console.log('TEST');
-      return <div>Loading...</div>;
-   }
+	if (idparam === undefined) {
+		console.log("TEST");
+		return <div>Loading...</div>;
+	}
 
-   return (
-      <div id='tweet-big-container'>
-         <ul id='viewtweet'>
-            {ownTweets.map((tweet, index) => (
-               <li className='tweet-container' key={index}>
-                  <p className='tweetp'>
-                     {tweet.username}{' '}
-                     <span id='time'>{returntimestamp(tweet)}</span>
-                  </p>
-                  <p className='tweetp'>{tweet.tweet}</p>
-                  <ul id='tweetfeatures'>
-                     <li></li>
-                  </ul>
-               </li>
-            ))}
-         </ul>
-      </div>
-   );
+	return <ShowTweetsContainer tweetsList={tweetsList} />;
 };
 
 export default OwnTweets;
