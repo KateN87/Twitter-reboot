2be78141301a0c74a6f5d941be1ab9faf65ad473
m-bengtsson <113067882+m-bengtsson@users.noreply.github.com>
2023-05-06 22:26:05 +0200

    post tweet mongodb

--- backend/src/main.js
+++ backend/src/main.js
@@ -11,16 +11,16 @@ import Tweet from './models/tweetModel.js';
 const app = express();
 app.use(express.static('public'));
 app.use(
-    cors({
-        origin: 'http://localhost:3000',
-    })
+   cors({
+      origin: 'http://localhost:3000',
+   })
 );
 
 //middleware
 app.use(express.json());
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+   console.log(req.path, req.method);
+   next();
 });
 
 app.use('/log', logRoutes);
@@ -29,106 +29,101 @@ app.use('/locked', lockedRoutes);
 //Routes
 
 app.get('/users', async (req, res) => {
-    const users = await User.find({});
-    res.send(users);
+   const users = await User.find({});
+   res.send(users);
 });
 
 app.get('/tweets', async (req, res) => {
-    const tweets = await Tweets.find({});
-    res.send(tweets);
+   const tweets = await Tweets.find({});
+   res.send(tweets);
 });
 
-app.get('/tweets/:user', (req, res) => {
-    const poster = req.params.user;
-    try {
-        const user = users.find(
-            (u) => u.username === poster || u.id === Number(poster)
-        );
-        if (!user) {
-            throw Error('No user with that name/id');
-        }
-        let newTweets = [];
-        for (let i = 0; i < tweets.length; i++) {
-            let tweet = tweets[i];
-            const tweetUser = tweet.username;
-            if (tweetUser === user.username) {
-                newTweets.push(tweet);
-            }
-        }
-        res.send(newTweets);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+app.get('/tweets/:user', async (req, res) => {
+   const poster = req.params.user;
+
+   try {
+      const user = await User.findOne({ $or: [{ username: poster }, { id: poster }] })
+      if (!user) {
+         throw Error('No user with that name/id');
+      }
+      console.log('user: ', user)
+      const tweets = await Tweet.find({ username: user.username })
+
+      console.log('tweets: ', tweets)
+      res.send(tweets);
+   } catch (error) {
+      res.status(400).json({ error: error.message });
+   }
 });
 
 app.get('/users/:user', (req, res) => {
-    const incomingUser = req.params.user;
-    //Kollar om den hittar användaren genom användarnamnet eller id:t
-    try {
-        const user = users.find(
-            (u) => u.username === incomingUser || u.id === Number(incomingUser)
-        );
-        if (user) {
-            res.status(200).send(user);
-        } else {
-            throw Error('User not found');
-        }
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+   const incomingUser = req.params.user;
+   //Kollar om den hittar användaren genom användarnamnet eller id:t
+   try {
+      const user = users.find(
+         (u) => u.username === incomingUser || u.id === Number(incomingUser)
+      );
+      if (user) {
+         res.status(200).send(user);
+      } else {
+         throw Error('User not found');
+      }
+   } catch (error) {
+      res.status(400).json({ error: error.message });
+   }
 });
 
 //KLAR GÄLLANDE MONGODB
 app.get('/trending', async (req, res) => {
-    try {
-        const tweets = await Tweet.find();
-        const occuringHashtags = {};
+   try {
+      const tweets = await Tweet.find();
+      const occuringHashtags = {};
 
-        // Iterate over each tweet
-        tweets.forEach(function (tweet) {
-            // Iterate over each hashtag in hashtag-array
-            tweet.hashtags.forEach(function (hashtag) {
-                const lowercaseHashtag = hashtag.toLowerCase();
-                // If the hashtag is already in occuringHashtags, add one to the count
-                if (occuringHashtags.hasOwnProperty(lowercaseHashtag)) {
-                    occuringHashtags[lowercaseHashtag]++;
-                } else {
-                    // If the hashtag is not there, add the hashtag and set it to one
-                    occuringHashtags[lowercaseHashtag] = 1;
-                }
-            });
-        });
-
-        // Takes each hashtag from occuringHashtags, and makes it into an array with objects
-        const occuringHashtagsArray = Object.keys(occuringHashtags).map(
-            (hashtag) => {
-                return {
-                    hashtag: hashtag,
-                    occurance: occuringHashtags[hashtag],
-                };
+      // Iterate over each tweet
+      tweets.forEach(function (tweet) {
+         // Iterate over each hashtag in hashtag-array
+         tweet.hashtags.forEach(function (hashtag) {
+            const lowercaseHashtag = hashtag.toLowerCase();
+            // If the hashtag is already in occuringHashtags, add one to the count
+            if (occuringHashtags.hasOwnProperty(lowercaseHashtag)) {
+               occuringHashtags[lowercaseHashtag]++;
+            } else {
+               // If the hashtag is not there, add the hashtag and set it to one
+               occuringHashtags[lowercaseHashtag] = 1;
             }
-        );
+         });
+      });
 
-        //Sort the array based on number, if b[1] is greater than a[1], then b is placed before a
-        occuringHashtagsArray.sort((a, b) => b.occurance - a.occurance);
-        const topFive = occuringHashtagsArray.slice(0, 5);
-        res.status(200).send(topFive);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+      // Takes each hashtag from occuringHashtags, and makes it into an array with objects
+      const occuringHashtagsArray = Object.keys(occuringHashtags).map(
+         (hashtag) => {
+            return {
+               hashtag: hashtag,
+               occurance: occuringHashtags[hashtag],
+            };
+         }
+      );
+
+      //Sort the array based on number, if b[1] is greater than a[1], then b is placed before a
+      occuringHashtagsArray.sort((a, b) => b.occurance - a.occurance);
+      const topFive = occuringHashtagsArray.slice(0, 5);
+      res.status(200).send(topFive);
+   } catch (error) {
+      res.status(400).json({ error: error.message });
+   }
 });
 
 app.get('/users/:id/:username', (req, res) => {
-    const id = +req.params.id;
-    const username = req.params.username;
-    console.log(username);
-    const i = users.findIndex((i) => i.id === id);
-    const followList = users[i].following;
-    if (followList.includes(username)) {
-        res.sendStatus(200);
-    } else {
-        res.sendStatus(404);
-    }
+   const id = +req.params.id;
+   const username = req.params.username;
+   console.log(username);
+   const i = users.findIndex((i) => i.id === id);
+   const followList = users[i].following;
+   if (followList.includes(username)) {
+      res.sendStatus(200);
+   } else {
+      res.sendStatus(404);
+   }
 });
 
 export { app };
